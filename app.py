d9404853ce034df648c1f1fc581e47ac2a0af527
Divyam Jain <43097792+Divyamdj@users.noreply.github.com>
2020-07-27 14:56:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,46 +19,41 @@ app = Flask(__name__)
 # routes
 @app.route('/', methods=['POST'])
 
+
 def predict():
-  # filename=open('test.txt')
-  # X=filename.read().split()
-  # X=np.array(X).astype(float)
-
-  # X_1=X[0:288].reshape(1,-1)
-  # X_1
-  # X_scaled=scaler.transform(X_1)
-  # X_pca=pca.transform(X_scaled)
-  # y=model.predict(X_pca)
-
-  # # send back to browser
-  # output = {'results': int([0])}
-
-  # # return data
-  # return jsonify(results=output)
-
-
-  # get data
-    data = request.get_json(force=True)
-
-    # convert data into dataframe
-    # data.update((x, [y]) for x, y in data.items())
-    # data_df = pd.DataFrame.from_dict(data)
-    X=data.split()
-	X=np.array(X).astype(float)
+	data = request.get_json(force=True)
+	X=data.split()
+	x=np.array(X).astype(float)
 
 	X_1=X[0:288].reshape(1,-1)
 	X_scaled=scaler.transform(X_1)
 	X_pca=pca.transform(X_scaled)
 	result=model.predict(X_pca)
 
-    # predictions
-    # result = model.predict(data_df)
+	output={'result':int(result[0])}
 
-    # send back to browser
-    output = {'results': int(result[0])}
+	return jsonify(results=output)
 
-    # return data
-    return jsonify(results=output)
+# def predict():
+#   	data = request.get_json(force=True)
+
+#  	# convert data into dataframe
+#  	X=data.split()
+# 	X=np.array(X).astype(float)
+
+# 	X_1=X[0:288].reshape(1,-1)
+# 	X_scaled=scaler.transform(X_1)
+# 	X_pca=pca.transform(X_scaled)
+# 	result=model.predict(X_pca)
+
+#  	# predictions
+#  	# result = model.predict(data_df)
+
+#  	# send back to browser
+#  	output = {'results': int(result[0])}
+
+#  	# return data
+#  	return jsonify(results=output)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
